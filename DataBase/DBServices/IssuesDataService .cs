d2e54bf3f1ffee63f9a832b5d290cdf16dc7d548
Chathurani Madushanka <Chathurani@CreativeSoftware.com>
2023-12-10 21:40:36 +0530

    add project

--- DataBase/DBServices/IssuesDataService .cs
+++ DataBase/DBServices/IssuesDataService .cs
@@ -27,7 +27,7 @@ namespace PollingScheduler.DataBase.DBServices
                             {
                                 int Locked = issue.Locked == true ? 1 : 0;
                                 string insertQuery =
-                                    $"insert into dbo.Issue values({issue.Id},{issue.user.Id},'{issue.Title}','{issue.State}',{Locked},'{issue.Created_At.ToString()}','{issue.Updated_At.ToString()}','{issue.Body}')";
+                                    $"insert into dbo.Issue values({issue?.Id},{issue?.user?.Id},'{issue?.Title}','{issue?.State}',{Locked},'{issue?.Created_At.ToString()}','{issue?.Updated_At.ToString()}','{issue.Body}')";
                                 using (SqlCommand command = new SqlCommand(insertQuery, connection))
                                 {
                                     command.ExecuteNonQuery();
